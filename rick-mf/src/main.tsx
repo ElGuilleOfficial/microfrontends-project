38cd942fdcc739c9b76292726554e4096ee68d79
ElGuilleOfficial <jgabrielguillen3000@gmail.com>
2025-04-11 15:25:40 -0400

    delete css

--- rick-mf/src/main.tsx
+++ rick-mf/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './i18n/index'
-import './index.css'
 import AppContainer from './components/AppContainer/Index'
 
 createRoot(document.getElementById('root')!).render(
